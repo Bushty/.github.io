e09b6e727035441bd0ce4a1761c574659ffedd4f
Bushty <bastian@gerkum.de>
2024-05-21 22:18:48 +0200

    Impressum added

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname.endsWith('.html')) {
+        window.history.replaceState(null, null, window.location.pathname.slice(0, -5));
+    }
+});

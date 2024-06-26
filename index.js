5eb26a7ef75af1d47996a8bb26dd1a2ed25de940
Bushty <bastian@gerkum.de>
2024-06-26 20:01:08 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // document.addEventListener('DOMContentLoaded', function() {
 //     if (window.location.pathname.endsWith('.html')) {
 //         window.history.replaceState(null, null, window.location.pathname.slice(0, -5));
-//     }
+//     } 
 // });
 
 // ---
